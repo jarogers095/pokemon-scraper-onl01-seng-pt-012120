0b6d7ec207b599513242970d462eea2f5affd001
James Rogers <jarogers095@gmail.com>
2020-03-24 07:04:18 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,12 @@
 class Pokemon
+  attr_reader(:id, :name, :type)
+  
+  def initialize(id = nil, name, type)
+    @id = id
+    @name = name
+    @type = type
+  end
+  
+  def self.save(name, type, db)
+    
 end
